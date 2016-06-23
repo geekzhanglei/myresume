e604bc634f581db19fea6d2734261f6a7bd15a79
zhanglei <41572876@qq.com>
2016-06-23 23:38:59 +0800

    profile completed for chrome

---
+++ js/directive.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*intro*/
+
+// adaptive height for every page
+var adaptiveHeight = function() {
+	return {
+		restrict: 'A',
+		link: function() {
+			$('.page').height($(window).height());
+		}
+	}
+}
+
+angApp
+	.directive('adaptiveHeight', adaptiveHeight)
